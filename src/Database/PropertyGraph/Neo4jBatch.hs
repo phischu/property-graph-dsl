ceaec774159e9050773c2222bf83a2a93b7a86d8
Philipp Schuster <pschuster@uni-koblenz.de>
2013-05-13 19:13:55 +0200

    scriptIO to tryIO

--- src/Database/PropertyGraph/Neo4jBatch.hs
+++ src/Database/PropertyGraph/Neo4jBatch.hs
@@ -10,13 +10,14 @@ import qualified Data.Aeson as JSON (Value(Array),eitherDecode,encode,toJSON)
 import Control.Monad.Writer.Lazy (WriterT,execWriterT,tell)
 import Control.Monad.State.Lazy (State,evalState,modify,get,lift)
 import Control.Monad.Free (Free(Pure,Free))
-import Control.Error (EitherT,runEitherT,scriptIO,hoistEither,fmapLT,noteT,hoistMaybe,left)
+import Control.Error (EitherT,runEitherT,tryIO,hoistEither,fmapLT,noteT,hoistMaybe,left)
+import Control.Exception (IOException)
 
 import qualified Data.Map as Map (fromList)
-import qualified Data.ByteString.Lazy.Char8 as BC (length)
 import qualified Data.Vector as Vector (fromList)
 import Data.Text (Text,unpack)
 import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy.Char8 as B (length)
 
 import Network.HTTP (Request(Request),simpleHTTP,getResponseCode,getResponseBody)
 import Network.HTTP.Base (ResponseCode,RequestMethod(POST))
@@ -75,20 +76,20 @@ add serveraddress propertygraph = runEitherT (do
     serveruri <- noteT (URIError "Failed to parse Server Address") (hoistMaybe (parseAbsoluteURI (unpack serveraddress)))
     apiuri    <- noteT (URIError "Failed to parse API Address")    (hoistMaybe (parseRelativeReference "db/data/batch"))
 
-    let request = Request uri verb headers body
+    let request = Request uri verb headers requestbody
         uri = (apiuri `relativeTo` serveruri)
         verb = POST
         headers = [
             mkHeader HdrAccept "application/json",
             mkHeader HdrContentType "application/json",
-            mkHeader HdrContentLength (show (BC.length body))]
-        body = JSON.encode (convertPropertyGraphToNeo4jBatch propertygraph)
+            mkHeader HdrContentLength (show (B.length requestbody))]
+        requestbody = JSON.encode (convertPropertyGraphToNeo4jBatch propertygraph)
 
-    (code,body) <- webInteract request `onFailure` InteractionError
+    (code,responsebody) <- webInteract request `onFailure` InteractionError
 
     case code of
-        (2,_,_) -> hoistEither (JSON.eitherDecode body) `onFailure` ResponseParseError
-        _       -> left (ResponseCodeError code body))
+        (2,_,_) -> hoistEither (JSON.eitherDecode responsebody) `onFailure` ResponseParseError
+        _       -> left (ResponseCodeError code responsebody))
 
 -- | Things that can go wrong when submitting a property graph to a neo4j database.
 data Neo4jBatchError = URIError String |
@@ -99,9 +100,9 @@ data Neo4jBatchError = URIError String |
 deriving instance Show Neo4jBatchError
 
 -- | Things that can go wrong when doing an http interaction.
-data InteractionError = UnexpectedError String |
+data InteractionError = UnexpectedError IOException |
                         ConnectionError ConnError |
-                        RetrievalError String
+                        RetrievalError IOException
 
 deriving instance Show InteractionError
 
@@ -109,10 +110,10 @@ type Body s = s
 
 webInteract :: (HStream s) => Request s -> EitherT InteractionError IO (ResponseCode,Body s)
 webInteract request = do
-    result   <- scriptIO (simpleHTTP request)               `onFailure` UnexpectedError
-    response <- hoistEither result                          `onFailure` ConnectionError
-    code     <- scriptIO (getResponseCode (Right response)) `onFailure` RetrievalError
-    body     <- scriptIO (getResponseBody (Right response)) `onFailure` RetrievalError
+    result   <- tryIO (simpleHTTP request)               `onFailure` UnexpectedError
+    response <- hoistEither result                       `onFailure` ConnectionError
+    code     <- tryIO (getResponseCode (Right response)) `onFailure` RetrievalError
+    body     <- tryIO (getResponseBody (Right response)) `onFailure` RetrievalError
     return (code,body)
 
 onFailure :: Monad m => EitherT a m r -> (a -> b) -> EitherT b m r
