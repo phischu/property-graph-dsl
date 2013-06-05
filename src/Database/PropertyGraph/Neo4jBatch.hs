bc95c5949356bad3b4ea50955e9f5b9c6586ea81
Philipp Schuster <pschuster@uni-koblenz.de>
2013-06-05 11:04:31 +0200

    well should work now

--- src/Database/PropertyGraph/Neo4jBatch.hs
+++ src/Database/PropertyGraph/Neo4jBatch.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings, StandaloneDeriving #-}
-module Database.PropertyGraph.Neo4jBatch () where
+module Database.PropertyGraph.Neo4jBatch (
+    runPropertyGraphT) where
 
 import Database.PropertyGraph.Internal (
     PropertyGraphT,Properties,Label,
@@ -7,17 +8,18 @@ import Database.PropertyGraph.Internal (
     TemporaryId(TemporaryId),PermanentUri(PermanentUri),
     PropertyGraphF(NewVertex,NewEdge))
 
-import Control.Proxy (Proxy,Producer,Consumer,request,respond,lift)
+import Control.Proxy (Proxy,Producer,Pipe,Consumer,request,respond,lift,(>->),runProxy)
 import Control.Proxy.Trans (liftP)
-import Control.Proxy.Trans.State (StateP,evalStateP,modify,get)
+import Control.Proxy.Morph (hoistP)
+import Control.Proxy.Trans.State (StateP,evalStateK,modify,get)
 
-import Control.Monad (forever,mzero)
+import Control.Monad (forever,mzero,(>=>),replicateM)
 import Control.Applicative ((<$>))
 import Control.Monad.IO.Class (MonadIO,liftIO)
 import Control.Monad.Trans.Free (FreeF(Pure,Free),runFreeT)
 import Control.Exception (IOException)
 
-import Control.Error (EitherT,fmapLT,runEitherT,tryIO)
+import Control.Error (EitherT,fmapLT,runEitherT,tryIO,left,fmapL)
 
 import Data.Aeson (
     ToJSON,toJSON,(.=),
@@ -28,7 +30,7 @@ import Data.Text (Text,append,pack,unpack)
 import Data.Map (Map)
 import Data.Vector (Vector)
 import qualified Data.Vector as Vector (mapM,toList,concat,singleton,empty)
-import qualified Data.Map as Map (union,fromList)
+import qualified Data.Map as Map (union,fromList,lookup,empty)
 
 import Network.Http.Client (
     Hostname,Port,openConnection,closeConnection,
@@ -42,78 +44,13 @@ import System.IO.Streams.Attoparsec (parseFromStream)
 
 import Network.URI (parseURI,uriPath)
 
-{-
-import Control.Proxy (Producer)
-
-import Database.PropertyGraph.Internal (
-    PropertyGraphT,PropertyGraph,
-    PropertyGraphF(NewVertex,NewEdge),VertexId(TemporaryId),
-    Properties,Label)
-
-import qualified Data.Aeson as JSON (Value(Array),encode,toJSON,json)
-
-import Control.Monad.Writer.Lazy (WriterT,runWriterT,tell)
-import Control.Monad.State.Lazy (StateT,evalStateT,modify,get,lift)
-import Control.Monad.Identity (Identity,runIdentity)
-import Control.Monad.Trans.Free (FreeF(Pure,Free),runFreeT)
-import Control.Error (EitherT,runEitherT,tryIO,fmapLT)
-import Control.Exception (IOException)
-
-import qualified Data.Map as Map (fromList)
-import Data.Monoid ((<>))
-import qualified Data.Vector as Vector (fromList)
-import Data.Text (Text,pack)
-
-
-
--- | Convert a given property graph to the equivalent body of a neo4j batch request.
-convertPropertyGraphToNeo4jBatch :: PropertyGraph a -> JSON.Value
-convertPropertyGraphToNeo4jBatch = snd . runIdentity . runPropertyGraphT
-
--- | Run a property graph transformer and yield the corresponding property graph
---   as a batch request suitable for neo4j as the second part of the result.
-runPropertyGraphT :: (Monad m,Functor m) => PropertyGraphT m a -> m (a,JSON.Value)
-runPropertyGraphT =
-    fmap (fmap (JSON.Array . Vector.fromList)) .
-    flip evalStateT 0 .
-    runWriterT .
-    interpretPropertyGraph
-
--- | Interpret a property graph as the list of neo4j requests and a running unique Id.
-interpretPropertyGraph :: (Monad m) => PropertyGraphT m a -> WriterT [JSON.Value] (StateT Integer m) a
-interpretPropertyGraph propertygraph = do
-
-    next <- lift (lift (runFreeT propertygraph))
-
-    case next of
-
-        Pure x -> return x
-
-        Free (NewVertex properties continue) -> do
-            VertexId vertexid <- lift (modify (+1) >> get) >>= return . VertexId
-            tell [vertexRequest properties vertexid]
-            interpretPropertyGraph (continue (VertexId vertexid))
-
-        Free (NewEdge properties label (VertexId from) (VertexId to) continue) -> do
-            runningid <- lift (modify (+1) >> get) >>= return
-            let tempid x = pack ("{" ++ show x ++ "}")
-            tell [edgeRequest properties label (tempid from) (tempid to) runningid]
-            interpretPropertyGraph continue
-
-
-
-
-
-
--}
-
-
 -- | Things that can go wrong when submitting a property graph to a neo4j database.
 data Neo4jBatchError = OpeningConnectionError IOException |
                        RequestBuildingError IOException |
                        EncodingError IOException |
                        RequestSendingError IOException |
                        ResponseReceivalError IOException |
+                       ResponseParseError String |
                        ConnectionClosingError IOException
 
 
@@ -123,11 +60,19 @@ data Neo4jBatchRequest = VertexRequest Properties TemporaryId
 data Neo4jBatchResponses = Neo4jBatchResponses (Vector Neo4jBatchResponse)
 data Neo4jBatchResponse = Neo4jBatchResponse Integer Text (Maybe Text) (Maybe JSON.Value)
 
-runPropertyGraphT :: (Proxy p,Monad m) => PropertyGraphT m r -> Producer p Neo4jBatchRequest m r
-runPropertyGraphT propertygraph = evalStateP 0 (interpretPropertyGraphT propertygraph)
+runPropertyGraphT :: (MonadIO m) => Hostname -> Port -> Int -> PropertyGraphT m r -> m r
+runPropertyGraphT hostname port n propertygraph = runProxy (evalStateK 0 (evalStateK Map.empty (
+    (liftP . (interpretPropertyGraphT propertygraph)) >->
+    chunk n >->
+    ((hoistP liftP) .) (insertPropertyGraphT hostname port))))
 
-interpretPropertyGraphT :: (Proxy p,Monad m) => PropertyGraphT m r -> Producer (StateP Integer p) Neo4jBatchRequest m r
-interpretPropertyGraphT propertygraph = do
+chunk :: (Proxy p,Monad m,Monad (Pipe p a [a] m)) => Int -> () -> Pipe p a [a] m r
+chunk n () = forever (do
+    xs <- replicateM n (request ())
+    respond xs)
+
+interpretPropertyGraphT :: (Proxy p,Monad m) => PropertyGraphT m r -> () -> Producer (StateP Integer p) Neo4jBatchRequest m r
+interpretPropertyGraphT propertygraph () = do
 
     next <- lift (runFreeT propertygraph)
 
@@ -139,13 +84,13 @@ interpretPropertyGraphT propertygraph = do
 
             temporaryid <- modify (+1) >> get >>= return . TemporaryId
             respond (VertexRequest properties temporaryid)
-            interpretPropertyGraphT (continue temporaryid)
+            interpretPropertyGraphT (continue temporaryid) ()
 
         Free (NewEdge properties label sourceid targetid continue) -> do
 
             modify (+1)
             respond (EdgeRequest properties label sourceid targetid)
-            interpretPropertyGraphT continue
+            interpretPropertyGraphT continue ()
 
 instance ToJSON Neo4jBatchRequest where
     toJSON (VertexRequest properties (TemporaryId vertexid)) = JSON.object [
@@ -182,26 +127,36 @@ vertexidURI (Permanent (PermanentUri vertexuri)) = vertexuri
 
 -- | Insert chunks of batch requests into a neo4j database...
 insertPropertyGraphT :: (Proxy p,MonadIO m) =>
-    Hostname -> Port -> () -> Consumer (StateP (Map TemporaryId PermanentUri) p) [JSON.Value] m r
+    Hostname -> Port -> () -> Consumer (StateP (Map TemporaryId PermanentUri) p) [Neo4jBatchRequest] m r
 insertPropertyGraphT hostname port () = forever (do
+
     requestvalues <- request ()
-    result <- liftIO (add hostname port (toJSON requestvalues))
+
+    permanentids <- get
+
+    result <- liftIO (add hostname port (replaceIds permanentids requestvalues))
+
     case result of
         Left e -> liftIO (print e)
         Right resultvalue -> do
             case matchTemporaryIds resultvalue of
                 Left e -> liftIO (print e)
-                Right vertexidmap -> modify (Map.union vertexidmap))
+                Right newpermanentids -> modify (Map.union newpermanentids))
 
-matchTemporaryIds :: JSON.Value -> Either String (Map TemporaryId PermanentUri)
-matchTemporaryIds batchresponsevalue = do
-    case fromJSON batchresponsevalue of
-        Error e -> Left e
-        Success batchresponse -> case batchresponse of
-            Neo4jBatchResponses vector -> do
-                pairs <- Vector.mapM responseToParing vector
-                return (Map.fromList (concat (Vector.toList pairs)))
-            _ -> Left "batch response not a vector"
+replaceIds :: (Map TemporaryId PermanentUri) -> [Neo4jBatchRequest] -> [Neo4jBatchRequest]
+replaceIds permanentids = map (replaceId permanentids)
+
+replaceId :: (Map TemporaryId PermanentUri) -> Neo4jBatchRequest -> Neo4jBatchRequest
+replaceId permanentids (EdgeRequest properties label sourceid targetid) =
+    EdgeRequest properties label (replace sourceid) (replace targetid) where
+        replace (Temporary temporaryid) =
+            maybe (Temporary temporaryid) Permanent
+                (Map.lookup temporaryid permanentids)
+
+matchTemporaryIds :: Neo4jBatchResponses -> Either String (Map TemporaryId PermanentUri)
+matchTemporaryIds (Neo4jBatchResponses vector) = do
+    pairs <- Vector.mapM responseToParing vector
+    return (Map.fromList (concat (Vector.toList pairs)))
 
 responseToParing :: Neo4jBatchResponse -> Either String [(TemporaryId,PermanentUri)]
 responseToParing (Neo4jBatchResponse temporaryid _ (Just completeuri) _) = do
@@ -212,7 +167,7 @@ responseToParing _ = Right []
 
 -- | Issue a JSON value representing a neo4j batch request to a neo4j database.
 --   Return the decoded response.
-add :: Hostname -> Port -> JSON.Value -> IO (Either Neo4jBatchError JSON.Value)
+add :: Hostname -> Port -> [Neo4jBatchRequest] -> IO (Either Neo4jBatchError Neo4jBatchResponses)
 add hostname port neo4jbatch = runEitherT (do
 
             connection <- tryIO (openConnection hostname port)
@@ -234,10 +189,14 @@ add hostname port neo4jbatch = runEitherT (do
                 parseFromStream JSON.json responsebody)))
                 `onFailure` ResponseReceivalError
 
+            responses <- case fromJSON jsonvalue of
+                Error e        -> left (ResponseParseError e)
+                Success result -> return result
+
             tryIO (closeConnection connection)
                 `onFailure` ConnectionClosingError
 
-            return jsonvalue)
+            return responses)
 
 deriving instance Show Neo4jBatchError
 
